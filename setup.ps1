494cd46388744580d7645226142011312061560e
ryo <ryo7000@gmail.com>
2021-07-06 20:14:18 +0900

    Update versions

--- setup.ps1
+++ setup.ps1
@@ -10,14 +10,14 @@ if (([System.Net.ServicePointManager]::SecurityProtocol -band [System.Net.Securi
 # vim
 if (!(Test-Path vim.exe -PathType Leaf)) {
   if ($FALSE) {
-    Invoke-WebRequest -Uri "https://github.com/koron/vim-kaoriya/releases/download/v8.2.0087-20200106/vim82-kaoriya-win64-8.2.0087-20200106.zip" -OutFile vim.zip
+    Invoke-WebRequest -Uri "https://github.com/koron/vim-kaoriya/releases/download/v8.2.1287-20200724/vim82-kaoriya-win64-8.2.1287-20200724.zip" -OutFile vim.zip
 
     Expand-Archive vim.zip .
     Move-Item vim82-kaoriya-win64\* .
     Remove-Item vim.zip
     Remove-Item vim82-kaoriya-win64
   } else {
-    Invoke-WebRequest -Uri "https://github.com/vim/vim-win32-installer/releases/download/v8.2.0940/gvim_8.2.0940_x64_signed.zip" -OutFile vim.zip
+    Invoke-WebRequest -Uri "https://github.com/vim/vim-win32-installer/releases/download/v8.2.3113/gvim_8.2.3113_x64_signed.zip" -OutFile vim.zip
 
     Expand-Archive vim.zip .
     Move-Item vim\vim82\* .
@@ -28,27 +28,27 @@ if (!(Test-Path vim.exe -PathType Leaf)) {
 
 # ripgrep
 if (!(Test-Path rg.exe -PathType Leaf)) {
-  Invoke-WebRequest -Uri "https://github.com/BurntSushi/ripgrep/releases/download/12.0.1/ripgrep-12.0.1-x86_64-pc-windows-msvc.zip" -OutFile ripgrep.zip
+  Invoke-WebRequest -Uri "https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-pc-windows-msvc.zip" -OutFile ripgrep.zip
 
   Expand-Archive ripgrep.zip .
-  Move-Item ripgrep-12.0.1-x86_64-pc-windows-msvc\rg.exe .
+  Move-Item ripgrep-13.0.0-x86_64-pc-windows-msvc\rg.exe .
   Remove-Item ripgrep.zip
-  Remove-Item ripgrep-12.0.1-x86_64-pc-windows-msvc -Recurse
+  Remove-Item ripgrep-13.0.0-x86_64-pc-windows-msvc -Recurse
 }
 
 # python3
 if (!(Test-Path python3 -PathType Container)) {
-  Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.8.3/python-3.8.3-embed-amd64.zip" -OutFile python3.zip
+  Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.6/python-3.9.6-embed-amd64.zip" -OutFile python3.zip
 
   Expand-Archive python3.zip python3
-  Expand-Archive python3\python38.zip python3
+  Expand-Archive python3\python39.zip python3
   Copy-Item python3\vcruntime140.dll .
   Copy-Item python3\vcruntime140_1.dll .
   Remove-Item python3.zip
 
   # for denite.nvim
   cd python3
-  (Get-Content python38._pth) -replace '#import site', 'import site' | Set-Content python38._pth
+  (Get-Content python39._pth) -replace '#import site', 'import site' | Set-Content python39._pth
   Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -OutFile get-pip.py
   .\python.exe get-pip.py
   .\python.exe -m pip install pynvim
